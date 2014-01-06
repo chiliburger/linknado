0689add1ba80f6f0b2e33728ebb959eaed5b72b2
Paul Coates <coatesp@gmail.com>
2014-01-05 22:53:22 -0800

    Created initial rails linknado application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Linknado::Application.config.secret_key_base = 'e0a815e8a247b51f2694fc98383a7643ee7d130a4a1d0b0ed997b7849d2af3a9bec726d4b1cd5b77c44cf122aa271b4d61329daf128f704673200f89c352a5b9'
