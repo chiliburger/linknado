5b01fd6eb338c11f97cd8f43363bfe8663dc1ef6
Paul Coates <coatesp@gmail.com>
2014-01-08 01:40:56 -0800

    Converted links scaffold controller tests to minitest spec.

--- test/controllers/links_controller_test.rb
+++ test/controllers/links_controller_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
 
-class LinksControllerTest < ActionController::TestCase
-  setup do
+describe LinksController do
+  before(:each) do
     @link = links(:one)
   end
 
-  test "should get index" do
+  it "should get index" do
     get :index
     assert_response :success
     assert_not_nil assigns(:links)
   end
 
-  test "should get new" do
+  it "should get new" do
     get :new
     assert_response :success
   end
 
-  test "should create link" do
+  it "should create link" do
     assert_difference('Link.count') do
       post :create, link: {  }
     end
@@ -24,22 +24,22 @@ class LinksControllerTest < ActionController::TestCase
     assert_redirected_to link_path(assigns(:link))
   end
 
-  test "should show link" do
+  it "should show link" do
     get :show, id: @link
     assert_response :success
   end
 
-  test "should get edit" do
+  it "should get edit" do
     get :edit, id: @link
     assert_response :success
   end
 
-  test "should update link" do
+  it "should update link" do
     patch :update, id: @link, link: {  }
     assert_redirected_to link_path(assigns(:link))
   end
 
-  test "should destroy link" do
+  it "should destroy link" do
     assert_difference('Link.count', -1) do
       delete :destroy, id: @link
     end
