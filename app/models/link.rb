33f928c435ffd1ede14b4961c4e267881ecfbb22
Paul Coates <coatesp@gmail.com>
2014-01-08 12:59:21 -0800

    Added default scope to the link model.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,6 @@
 class Link < ActiveRecord::Base
   validates_presence_of :name, :uri, :username
   validates_numericality_of :points, :only_integer => true, :greater_than_or_equal_to => 0
+
+  default_scope { order('points Desc') }
 end
