3f5977b7365f2934338d849d1c7cccfe442c56a1
Paul Coates <coatesp@gmail.com>
2014-01-08 01:37:44 -0800

    Added site index page.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,6 @@ Linknado::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -55,4 +52,8 @@ Linknado::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  #
+  # You can have the root of your site routed with "root"
+  root 'site#index'
+
 end
