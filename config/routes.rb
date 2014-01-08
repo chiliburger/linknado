d8291af06c151eeb779306624f32ab88f6a7f8ac
Paul Coates <coatesp@gmail.com>
2014-01-08 15:11:07 -0800

    Changed to not have comments and points nested resources under links.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Linknado::Application.routes.draw do
-  resources :links do
-    resources :comments
-    resources :points
-  end
+  resources :comments
+  resources :links
+  resources :points
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
